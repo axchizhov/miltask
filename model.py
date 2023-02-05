90831372fca5dd603095fff3ffce5c8c9b784dfb
axchizhov <66429249+axchizhov@users.noreply.github.com>
2023-02-05 03:29:22 +0300

    added classificator, conf matrix

--- model.py
+++ model.py
@@ -11,7 +11,8 @@ class Autoencoder(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 12, 3, stride=2, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(12, 4, 3, stride=2, padding=1)
+            nn.Conv2d(12, 4, 3, stride=2, padding=1),
+            nn.LeakyReLU()
             )
         
         self.code = None
@@ -28,3 +29,22 @@ class Autoencoder(nn.Module):
         decoded = self.decoder(coded)
 
         return decoded
+
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.clf = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10),
+        )
+
+    def forward(self, x):
+        x = self.clf(x)
+        
+        return x
+
